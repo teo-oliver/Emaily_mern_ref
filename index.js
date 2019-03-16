c7f33e8122267e6aa13c178ba7106f71a9e572a7
Teo Oliver <teo.liz.oli@gmail.com>
2019-03-16 10:41:38 +0000

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ hello: "there it is" });
 });
 
 const PORT = process.env.PORT || 3000;
